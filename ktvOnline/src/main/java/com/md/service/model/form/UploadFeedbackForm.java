68127d86276aa979b1569a6f0f90dffbb00d34f2
seymourtang <tangxianmeng@live.com>
2023-10-12 17:26:44 +0800

    chore(validation):adjust UploadFeedbackForm validation

--- ktvOnline/src/main/java/com/md/service/model/form/UploadFeedbackForm.java
+++ ktvOnline/src/main/java/com/md/service/model/form/UploadFeedbackForm.java
@@ -2,7 +2,6 @@ package com.md.service.model.form;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Map;
@@ -15,9 +14,7 @@ public class UploadFeedbackForm {
     @NotNull
     private List<String> tags;
 
-    @NotBlank(message = "description cannot be empty")
     private String description;
 
-    @NotBlank(message = "logURL cannot be empty")
     private String logURL;
 }
